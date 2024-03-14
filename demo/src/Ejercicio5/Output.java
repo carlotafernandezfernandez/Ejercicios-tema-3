78e0db88d73db12d5993e50604ceb8c2ef968a1f
carlotafernandezfernandez <159237397+carlotafernandezfernandez@users.noreply.github.com>
2024-03-14 10:51:31 +0000

    casi casi

--- demo/src/Ejercicio5/Output.java
+++ demo/src/Ejercicio5/Output.java
@@ -2,7 +2,7 @@ public class Output {
     public imprimirIMC(){;
         if (Persona.valorarPesoCorporal(Persona.calcularIMC(getPeso(),getAltura())==1)){
             System.out.println("Tiene sobrepeso");  
-        }if (p.valorarPesoCorporal(p.calcularIMC(p.getPeso(), p.getAltura())==-1)){
+        }if (valorarPesoCorporal(calcularIMC(getPeso(),getAltura())==-1)){
             System.out.println("Su peso esta por debajo de lo normal");
         }else{
             System.out.println("Su peso es normal");
@@ -11,7 +11,7 @@ public class Output {
     }
 
     public void Adulto (){
-        if(p.esMayorDeEdad(p.getEdad())){
+        if(esMayorDeEdad(p.getEdad())){
             System.out.println("Es mayor de edad");
         }else{
             System.out.println("Es menor de edad");
